8d4df8b2d5e0f1ed4da79a0b75e5225addf262e6
835431078@qq.com <835431078@qq.com>
2019-10-28 00:10:56 +0800

    2019-10-28 commit

--- src/main/java/com/netty/study/example/sty013/DeOrEnCoder.java
+++ src/main/java/com/netty/study/example/sty013/DeOrEnCoder.java
@@ -1,7 +1,16 @@
 package com.netty.study.example.sty013;
 
 import javax.activation.MailcapCommandMap;
+import java.io.File;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Base64;
 
 public class DeOrEnCoder {
     //编码或解码
@@ -14,6 +23,32 @@ public class DeOrEnCoder {
         RandomAccessFile inputRan = new RandomAccessFile("", "r");
 
         RandomAccessFile outRan = new RandomAccessFile("", "rw");
+
+        long inpuLen = new File(inputFile).length();
+
+        FileChannel inputFileChannel = inputRan.getChannel();
+        FileChannel outFileChannel = outRan.getChannel();
+
+        MappedByteBuffer inputData = inputFileChannel.map(FileChannel.MapMode.READ_WRITE, 0, inpuLen);
+
+        Charset charset = Charset.forName("utf-8");
+
+        CharsetDecoder charsetDecoder = charset.newDecoder();
+        CharsetEncoder charsetEncoder = charset.newEncoder();
+
+        CharBuffer charBuffer = charsetDecoder.decode(inputData);
+
+        ByteBuffer outByteBuffer = charsetEncoder.encode(charBuffer);
+
+        outFileChannel.write(outByteBuffer);
+
+        outRan.close();
+
+        Charset.availableCharsets().forEach((k,v) ->{
+            System.out.println(k + ":" + v);
+        });
+
+
 
 
 
--- src/main/java/com/netty/study/example/sty013/readme.md
+++ src/main/java/com/netty/study/example/sty013/readme.md
@@ -17,3 +17,47 @@ nio 网络操作
 异步编程 event
 
 消息量不是特别大，连接特别多。一个线程可以实现多个连接
+
+
+### 字符集的编码 
+
+utf-16
+utf-32
+
+unicode 表示范围最大的
+
+ascii  码  
+    7bit表示一个字符，可表示128种字符
+iso88569-1
+    8bite表示一个字符，即占用一个字符。共计可表示256个字符。向下兼容 ascii
+    
+gb2312 
+    对常用汉字进行编码。2byte表示一个字节
+gbk
+    包含gb2312和一些生僻词
+gb18030
+    最完成的汉字表示。表示的汉字最多
+台湾的编码 big5(繁体)
+
+
+全球各个国家的编码，unicode
+    采用两个字节表示一个字符。存储成本高。
+utf8
+
+utf16
+
+utf32
+
+utf unicode translation format
+
+unicode是一种编码方式，utf是一种存储方式。utf是unicode的实现方式之一
+utf16 le(little endian)  utf16 be(big endian)
+在文件的开头加上标示的字符 oxfeff oxfffe
+
+utf8变长的字节表示形式
+    对于中文，会通过3个字节来表示，。最多可以使用6个字节来表示
+    
+    
+    字节序bom (byte order mark)在一个文件的其实位置上加标示，
+    windows下文件够默认会有不可见的字符，解析的时候要平台区分
+
