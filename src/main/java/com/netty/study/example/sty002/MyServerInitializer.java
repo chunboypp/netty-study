969eba1678bb564be895e699e4279f9b84d362d7
yepengpeng <yepengpeng@chunbo.com>
2020-05-19 18:45:38 +0800

    2020-05-19 commit

--- src/main/java/com/netty/study/example/sty002/MyServerInitializer.java
+++ src/main/java/com/netty/study/example/sty002/MyServerInitializer.java
@@ -21,7 +21,7 @@ public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        ByteBuf
+
         ChannelPipeline pipeline = ch.pipeline();
         pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,0,4,0,4));
         pipeline.addLast(new LengthFieldPrepender(4));
